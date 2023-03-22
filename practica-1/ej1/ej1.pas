1af1731642803daa877d3af756fa767c44b7d9c7
santi-nihany <santi.nihany@gmail.com>
2023-03-22 11:40:21 -0300

    initial commit: practica-1

---
+++ practica-1/ej1/ej1.pas
@@ -0,0 +1,25 @@
+
+Program EJ1;
+
+Const 
+    FIN =   'cemento';
+
+Var 
+    nomArchivo:   String;
+    nomMaterial:   String;
+    archivo:   Text;
+Begin
+    WriteLn('Ingrese el nombre del archivo: ');
+    ReadLn(nomArchivo);
+    Assign(archivo, nomArchivo);
+    ReWrite(archivo);
+    WriteLn('Ingrese el nombre del material:' );
+    ReadLn(nomMaterial);
+    While (nomMaterial <> FIN ) Do
+        Begin
+            Write(archivo,nomMaterial +'-');
+            WriteLn('Ingrese el nombre del material: ');
+            ReadLn(nomMaterial);
+        End;
+    Close(archivo);
+End.
